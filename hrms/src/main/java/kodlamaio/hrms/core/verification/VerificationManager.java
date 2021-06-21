06a9454ecac43c0e0ee7510ca8df0fe075bbc5c0
Kerem Candangil <keremc4306@gmail.com>
2021-06-21 22:30:03 +0300

    Verification codes have been added.

--- hrms/src/main/java/kodlamaio/hrms/core/verification/VerificationManager.java
+++ hrms/src/main/java/kodlamaio/hrms/core/verification/VerificationManager.java
@@ -1,11 +1,24 @@
 package kodlamaio.hrms.core.verification;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.dataAccess.abstracts.VerificationCodeDao;
+import kodlamaio.hrms.entities.concretes.VerificationCode;
 
 @Service
 public class VerificationManager implements VerificationService {
+	
+	private VerificationCodeDao verificationCodeDao;
+
+	@Autowired
+	public VerificationManager(VerificationCodeDao verificationCodeDao) {
+		super();
+		this.verificationCodeDao = verificationCodeDao;
+	}
 
 	@Override
 	public void sendLink(String email) {
@@ -16,9 +29,11 @@ public class VerificationManager implements VerificationService {
 	}
 
 	@Override
-	public void sendCode(String email) {
+	public void sendCode(String email, int id) {
 		UUID uuid = UUID.randomUUID();
 		String verfCode = uuid.toString();
+		VerificationCode code = new VerificationCode(id, verfCode, false, LocalDate.now());
+		this.verificationCodeDao.save(code);
 		System.out.println("Verification link has been sent to " + email);
 		System.out.println("Your activation code: " + verfCode);	
 	}
---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/VerificationCode.java
@@ -0,0 +1,46 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "verification_codes")
+public class VerificationCode {
+
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="user_id")
+	private int userId;
+	
+	@Column(name="code")
+	private String code;
+	
+	@Column(name="is_confirmed")
+	private boolean isConfirmed;
+	
+	@Column(name="created_at", columnDefinition = "Date defult CURRENT_DATE")
+	private LocalDate createAt = LocalDate.now();
+	
+	public VerificationCode(int userId, String code, boolean isConfirmed, LocalDate createAt) {
+		super();
+		this.userId = userId;
+		this.code = code;
+		this.isConfirmed = isConfirmed;
+		this.createAt = createAt;
+	}
+}
